0cfc1551ac12f9482c99076b1b4e56cb01215720
Danielle Heberling <deeheber@users.noreply.github.com>
2025-01-26 07:43:06 -0800

    Refactor a little bit.

--- bin/small-talk.ts
+++ bin/small-talk.ts
@@ -4,8 +4,8 @@ import { App } from 'aws-cdk-lib'
 import { SmallTalkStack } from '../lib/small-talk-stack'
 
 const app = new App()
-new SmallTalkStack(app, 'SmallTalkStack', {
-  description: 'Small Talk CDK Stack',
+new SmallTalkStack(app, 'small-talk', {
+  description: 'Resources needed for small talk',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
